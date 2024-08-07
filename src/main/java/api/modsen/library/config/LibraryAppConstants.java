6ca59eeef26dd08b1f07439d524e93c027ea65b8
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-08-07 23:02:20 +0300

    Library service with methods to find available books

--- src/main/java/api/modsen/library/config/LibraryAppConstants.java
+++ src/main/java/api/modsen/library/config/LibraryAppConstants.java
@@ -8,9 +8,10 @@ public class LibraryAppConstants {
     public static final String BLANK_GENRE_MESSAGE = "Genre cannot be blank";
     public static final String BLANK_DESCRIPTION_MESSAGE = "Description cannot be blank";
     public static final String BLANK_AUTHOR_MESSAGE = "Author cannot be blank";
-    public static final short MAX_LENGTH_OF_BOOK_TITLE = 50;
-    public static final short MAX_LENGTH_OF_BOOK_DESCRIPTION = 1000;
-    public static final short MAX_LENGTH_OF_BOOK_GENRE_AND_AUTHOR = 255;
+    public static final int MAX_LENGTH_OF_BOOK_TITLE = 50;
+    public static final int MAX_LENGTH_OF_BOOK_DESCRIPTION = 1000;
+    public static final int MAX_LENGTH_OF_BOOK_GENRE_AND_AUTHOR = 255;
     public static final String BOOK_NOT_FOUND_MESSAGE_WITH_ID = "Book not found with id: ";
     public static final String BOOK_NOT_FOUND_MESSAGE_WITH_ISBN = "Book not found with isbn: ";
+    public static final int MAX_NUMBER_OF_DAYS_BORROW_BOOK = 30;
 }
--- src/main/java/api/modsen/library/services/LibraryService.java
+++ src/main/java/api/modsen/library/services/LibraryService.java
@@ -1,17 +1,73 @@
 package api.modsen.library.services;
 
+
 import api.modsen.library.entities.book.Book;
 import api.modsen.library.entities.library.BookStatus;
 import api.modsen.library.repositories.BookStatusRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+
+
+import static api.modsen.library.config.LibraryAppConstants.MAX_NUMBER_OF_DAYS_BORROW_BOOK;
+
 
 @Service
 public class LibraryService {
     @Autowired
     private BookStatusRepository bookStatusRepository;
 
-    public BookStatus addBookStatus(Book book) {
 
+    @Async
+    public CompletableFuture<BookStatus> addBookStatus(Book book) {
+        LocalDateTime borrowAt = LocalDateTime.now();
+        LocalDateTime returnAt = generateRandomReturnTime(borrowAt);
+
+
+        BookStatus bookStatus = new BookStatus();
+        bookStatus.setBook(book);
+        bookStatus.setBorrowedAt(borrowAt);
+        bookStatus.setReturnAt(returnAt);
+
+
+        return CompletableFuture.completedFuture(bookStatusRepository.save(bookStatus));
+    }
+
+
+    private LocalDateTime generateRandomReturnTime(LocalDateTime borrowAt) {
+        Random random = new Random();
+        int borrowDays = random.nextInt(MAX_NUMBER_OF_DAYS_BORROW_BOOK) + 1;
+        return borrowAt.plusDays(borrowDays);
+    }
+
+
+    public List<BookStatus> findAvailableBooks() {
+        List<BookStatus> availableBooks = new ArrayList<>();
+        List<BookStatus> bookStatuses = bookStatusRepository.findAll();
+
+
+        for (BookStatus bookStatus: bookStatuses) {
+            if (isBookAvailable(bookStatus)) {
+                availableBooks.add(bookStatus);
+            }
+        }
+
+
+        return availableBooks;
+    }
+
+
+    public boolean isBookAvailable(BookStatus bookStatus) {
+        LocalDateTime now = LocalDateTime.now();
+        return now.isAfter(bookStatus.getReturnAt());
     }
 }
+
+
