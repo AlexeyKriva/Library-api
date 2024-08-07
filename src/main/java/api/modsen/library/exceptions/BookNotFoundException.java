9822ba94fe06004a3068303e6e79f5cae62bcd96
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-08-07 15:06:10 +0300

    Book not found exception

---
+++ src/main/java/api/modsen/library/exceptions/BookNotFoundException.java
@@ -0,0 +1,7 @@
+package api.modsen.library.exceptions;
+
+public class BookNotFoundException extends RuntimeException{
+    public BookNotFoundException(String message) {
+        super(message);
+    }
+}
