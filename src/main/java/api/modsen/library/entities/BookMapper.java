6eb83d520494d0e79f337112c4f26f0f731e9d0b
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-08-07 15:09:00 +0300

    Book mapper and dto

---
+++ src/main/java/api/modsen/library/entities/BookMapper.java
@@ -0,0 +1,10 @@
+package api.modsen.library.entities;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BookMapper {
+    BookMapper INSTANCE = Mappers.getMapper(BookMapper.class);
+    Book fromBookDtoToBook(BookDto bookDto);
+}
