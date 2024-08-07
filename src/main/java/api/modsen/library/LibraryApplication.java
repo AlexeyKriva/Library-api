145881a40085801d4ab177b8ffe53eab9abb4935
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-08-07 13:25:10 +0300

    Initialization project

---
+++ src/main/java/api/modsen/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package api.modsen.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
+
+}
