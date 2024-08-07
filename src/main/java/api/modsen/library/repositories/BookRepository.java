cfaaf7f979ab4cd6bdc83e57ec2232f53d81a9fa
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-08-07 14:03:37 +0300

    Book repository

--- src/main/java/api/modsen/library/repositories/BookRepository.java
+++ src/main/java/api/modsen/library/repositories/BookRepository.java
@@ -1,2 +1,15 @@
-package api.modsen.library.repositories;public interface BookRepository {
+package api.modsen.library.repositories;
+
+import api.modsen.library.entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BookRepository extends JpaRepository<Long, Book> {
+    Optional<Book> findById(long id);
+    Optional<Book> findByIsbn(String isbn);
+    Book save();
 }
