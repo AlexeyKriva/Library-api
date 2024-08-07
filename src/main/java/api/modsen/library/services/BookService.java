c5d7ead1f425bfd9ccacf2774c50f1b05303b0b7
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-08-07 18:59:56 +0300

    Update book service

--- src/main/java/api/modsen/library/services/BookService.java
+++ src/main/java/api/modsen/library/services/BookService.java
@@ -8,11 +8,13 @@ import api.modsen.library.exceptions.BookNotFoundException;
 import api.modsen.library.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class BookService {
     @Autowired
     private BookRepository bookRepository;
