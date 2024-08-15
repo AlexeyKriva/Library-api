b567e9b9302ba6a22cf0261908513cf9d57ceed4
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-08-15 23:30:05 +0300

    Docker-compose and fixes

--- api-gateway/src/main/java/api/modsen/library/apigateway/config/GatewayConstants.java
+++
@@ -1,5 +0,0 @@
-package api.modsen.library.apigateway.config;
-
-public class GatewayConstants {
-    public static final String EXCEPTION_MESSAGE = "Please wait server load or reload page.";
-}
--- book-microservice/src/main/resources/db.migration/V3__Create_Library_Table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE library
-(
-    id          SERIAL PRIMARY KEY,
-    book_id     BIGINT    NOT NULL,
-    borrowed_at TIMESTAMP NOT NULL,
-    return_at   TIMESTAMP NOT NULL,
-    CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES books (id) ON DELETE CASCADE
-)
